d231a184fb1cba45e0a149c02469e340d0260059
lburgazzoli <lburgazzoli@gmail.com>
2015-02-26 21:24:55 +0100

    fix

--- lb-docker/centos-systemd/install-docker-64.sh
+++ lb-docker/centos-systemd/install-docker-64.sh
@@ -8,7 +8,7 @@ if [ ! -d /usr/local/lib/systemd/system ]; then
     mkdir -p /usr/local/lib/systemd/system
 fi
 
-curl -L https://get.docker.com/builds/Linux/x86_64/docker-latest > /usr/local/bin/docker; 
+curl -L https://get.docker.com/builds/Linux/x86_64/docker-latest > /usr/local/bin/docker
 chmod ug+x /usr/local/bin/docker
 
 cp systemd-docker.service /usr/local/lib/systemd/system/docker.service
