30ed4ee63fe33eea87e04b97e6e7be98a60cc237
lburgazzoli <lburgazzoli@gmail.com>
2016-08-30 09:51:00 +0200

    fix

--- resources/bootstrap.sh
+++ resources/bootstrap.sh
@@ -41,6 +41,8 @@ function install_java {
 ######################################################################################
 
 if [ -f /etc/debian_version ]; then
+    export DEBIAN_FRONTEND=noninteractive
+
     # debian / ubuntu
     apt-get -y update
     #apt-get install --no-install-recommends -y python-software-properties
@@ -74,7 +76,7 @@ V_GRADLE=3.0
 
 get http://apache.fastbull.org/maven/maven-3/${V_MAVEN}/binaries/apache-maven-${V_MAVEN}-bin.tar.gz /tmp/maven-${V_MAVEN}.tar.gz
 get https://services.gradle.org/distributions/gradle-${V_GRADLE}-bin.zip /tmp/gradle-${V_GRADLE}.zip
-get https://raw.githubusercontent.com/lburgazzoli/lb-devops/master/lb-vagrant/resources/bashrc /home/vagrant/.bashrc
+get https://raw.githubusercontent.com/lburgazzoli/lb-vagrant/master/resources/bashrc /home/vagrant/.bashrc
 
 install_tgz /opt/tools/maven /tmp/maven-${V_MAVEN}.tar.gz
 
